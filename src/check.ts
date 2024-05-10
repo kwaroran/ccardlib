61a496e6b8bf07cd01ad5f6c1b1cfee28a1ba75e
kwaroran <amasio1234@proton.me>
2024-05-10 15:15:18 +0900

    Change to ajv

--- src/check.ts
+++ src/check.ts
@@ -1,18 +1,19 @@
 
-import {Validator} from 'jsonschema';
+import { Ajv } from 'ajv';
 import { CCSchemas } from './schemas.js';
 
 export function checkCharacterCardVersion(data: any): 'v1'|'v2'|'v3'|'unknown'{
     
-    const v = new Validator();
-    if(v.validate(data, CCSchemas.V3).valid){
-        return 'v3';
+    const v = new Ajv();
+
+    if(v.compile(CCSchemas.V3)(data)){
+        return 'v3'
     }
-    if(v.validate(data, CCSchemas.V2).valid){
-        return 'v2';
+    if(v.compile(CCSchemas.V2)(data)){
+        return 'v2'
     }
-    if(v.validate(data, CCSchemas.V1).valid){
-        return 'v1';
+    if(v.compile(CCSchemas.V1)(data)){
+        return 'v1'
     }
 
     return 'unknown';
